5c8cf01aebb1309645f3cb0051dc62f3df1872c8
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-19 11:26:47 +0600

    ad error try catch and finaly

---
+++ try-case.js
@@ -0,0 +1,17 @@
+
+console.log('ami')
+console.log('Tomay')
+console.log('VAllo')
+console.log('Basi')
+try {
+
+    console.log(student)
+}
+catch (e) {
+    console.log('ami kinto error mamabose asi tmiamay vole jeo na')
+}
+
+
+console.log('tinu')
+console.log('ki ')
+console.log('vallo vaso')
