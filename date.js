a7074130fde3975579bcadb67bb5342b1b104e6c
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-19 21:30:17 +0600

    ad to the all part

---
+++ date.js
@@ -0,0 +1,13 @@
+let a = new Date()
+
+// console.log(a.getDay());
+
+class Vegetable {
+    constructor(targetPlanet) {
+        this.targetPlanet
+    }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // 
